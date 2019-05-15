933182a3fec1390c2e731e621d4c482dec36e4ef
Harsh Anand <harsh19anand@gmail.com>
2019-05-15 10:58:45 +0530

    CalculatorActivity Page Added.

--- app/src/main/java/com/example/cimfrblastguide/MainActivity.java
+++ app/src/main/java/com/example/cimfrblastguide/MainActivity.java
@@ -1,13 +1,27 @@
 package com.example.cimfrblastguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button CalculatorMainActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CalculatorMainActivity = findViewById(R.id.btnCalculatorMainActivity);
+        CalculatorMainActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
