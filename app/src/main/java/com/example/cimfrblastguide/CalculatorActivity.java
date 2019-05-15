6faeaba4f5a2c57c4a1c156c06d905ce3dbdcab9
Harsh Anand <harsh19anand@gmail.com>
2019-05-15 11:20:03 +0530

    CalculatorActivity Button Added and Linked.

--- app/src/main/java/com/example/cimfrblastguide/CalculatorActivity.java
+++ app/src/main/java/com/example/cimfrblastguide/CalculatorActivity.java
@@ -1,13 +1,48 @@
 package com.example.cimfrblastguide;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class CalculatorActivity extends AppCompatActivity {
+
+    private Button volumeOfRock;
+    private Button Vibration;
+    private Button MassCharge;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_calculator);
+
+        Vibration = (Button)findViewById(R.id.btnVibrationCalculatorActivity);
+        Vibration.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculatorActivity.this, VibrationActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        volumeOfRock = (Button)findViewById(R.id.btnVolCalCalculatorActivity);
+        volumeOfRock.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculatorActivity.this, VolumeOfRockActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        MassCharge = (Button)findViewById(R.id.btnMassChargeCalculatorActivity);
+        MassCharge.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(CalculatorActivity.this, MassChargeActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
 }
