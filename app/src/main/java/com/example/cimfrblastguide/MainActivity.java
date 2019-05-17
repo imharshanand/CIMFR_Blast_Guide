780f46e0fb12ff6537809076972708dc7ab69838
Harsh Anand <harsh19anand@gmail.com>
2019-05-17 09:30:56 +0530

    Blast Layout Activity Added.

--- app/src/main/java/com/example/cimfrblastguide/MainActivity.java
+++ app/src/main/java/com/example/cimfrblastguide/MainActivity.java
@@ -9,17 +9,27 @@ import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     private Button CalculatorMainActivity;
+    private Button BlastHoleActivity;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        CalculatorMainActivity = findViewById(R.id.btnCalculatorMainActivity);
+        CalculatorMainActivity = (Button)findViewById(R.id.btnCalculatorMainActivity);
         CalculatorMainActivity.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent = new Intent(MainActivity.this, CalculatorActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        BlastHoleActivity = (Button)findViewById(R.id.btnBlastActivity);
+        BlastHoleActivity.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, BlastHolesActivity.class);
                 startActivity(intent);
             }
         });
