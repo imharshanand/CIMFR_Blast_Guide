f6606e15465a304036915e48543b1eab43461367
Harsh Anand <harsh19anand@gmail.com>
2019-05-29 15:25:34 +0530

    PDF Display Implimented. About Department page ready.

--- app/src/main/java/com/example/cimfrblastguide/AboutDepartment.java
+++ app/src/main/java/com/example/cimfrblastguide/AboutDepartment.java
@@ -3,11 +3,15 @@ package com.example.cimfrblastguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class AboutDepartment extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_department);
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("pdf_department.pdf").load();
     }
 }
