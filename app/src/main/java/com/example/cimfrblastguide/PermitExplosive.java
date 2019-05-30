c91b7f36c50368c2d0d92f0ef0d968139d1f5a97
Harsh Anand <harsh19anand@gmail.com>
2019-05-30 15:21:09 +0530

    Permitted explosive made in PDF reader format. Minor Changes.

--- app/src/main/java/com/example/cimfrblastguide/PermitExplosive.java
+++ app/src/main/java/com/example/cimfrblastguide/PermitExplosive.java
@@ -3,11 +3,16 @@ package com.example.cimfrblastguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class PermitExplosive extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permit_explosive);
+
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("permitted_explosive.pdf").load();
     }
 }
