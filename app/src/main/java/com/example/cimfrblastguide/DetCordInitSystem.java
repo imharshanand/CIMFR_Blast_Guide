18257e0bdc2f54cd544c6860ba7a0ebaff389bcf
Harsh Anand <harsh19anand@gmail.com>
2019-06-04 16:42:31 +0530

    PDF to DetCordInitSystem added and CalculatorActivity now scrollable.

--- app/src/main/java/com/example/cimfrblastguide/DetCordInitSystem.java
+++ app/src/main/java/com/example/cimfrblastguide/DetCordInitSystem.java
@@ -3,11 +3,16 @@ package com.example.cimfrblastguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class DetCordInitSystem extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_det_cord_init_system);
+
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("detonaing_cord.pdf").load();
     }
 }
