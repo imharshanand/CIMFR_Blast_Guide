e0babc8393c5b72a85a77b143a2006cb2139ccf9
Harsh Anand <harsh19anand@gmail.com>
2019-06-17 10:35:18 +0530

    Added about developers page.

--- app/src/main/java/com/example/cimfrblastguide/AboutDevelopers.java
+++ app/src/main/java/com/example/cimfrblastguide/AboutDevelopers.java
@@ -3,11 +3,16 @@ package com.example.cimfrblastguide;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.github.barteksc.pdfviewer.PDFView;
+
 public class AboutDevelopers extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_developers);
+
+        PDFView pdfView = (PDFView) findViewById(R.id.pdfView);
+        pdfView.fromAsset("developed_by.pdf").load();
     }
 }
